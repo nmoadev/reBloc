1647bfc4aded212e121eeda60e3fd3c7b7b65269
nmoadev <alain.kuchta@gmail.com>
2015-04-16 18:34:25 -0400

    More progress on game flow

--- rebloc.js
+++ rebloc.js
@@ -1,59 +1,79 @@
-var rebloc = {},
+var Math = require('math'),
+    EventEmitter = require('events').EventEmitter,
+    rebloc = {},
     _rebloc;
 
-_rebloc = {
-  rounds: 0,
-  delay: 1,
-  state: "Setup"
-};
+utils.inherits(rebloc, EventEmitter);
 
 rebloc.start = function start(trellis) {
+  _rebloc = {
+    rounds: 0, // use to track progess and decide length of sequence
+    delay: 700, // 700 ms is starting delay, this would lead to 700 rounds... we'll work it out
+    sequence: [],
+    seqIdx: 0
+    state: "Setup"
+  };
 
-
-
+  _rebloc.onNextRound();
 };
 
+_rebloc.randomIndex = function randomIndex() {
+  return {
+    i : Math.floor(Math.random() * (3 - 0 + 1) + 0),
+    j : Math.floor(Math.random() * (3 - 0 + 1) + 0)
+  };
+};
 
-// rebloc.acquire = function acquire(button) {
-//   _rebloc.currentAcquire++;
-//   _rebloc.lastTimeout = setTimeout(function() {
-//     if (_rebloc.buttonPressed === false) {
-//       rebloc.emit('gameover');
-//     }
-//   }, _rebloc.delay);
-// };
-//
-//
-// trellis.on('buttonPress', function(buttonNumber) {
-//   // If this button press gives us the button we were looking for
-//   if (buttonNumber === rebloc.currentButton()) {
-//     // cancel the last acquisition
-//     clearTimeout(_trellis.lastTimeout);
-//     // start the next acquisition, if we have another button to acquire
-//     if (rebloc.nextButton() {
-//       rebloc.acquire(rebloc.currentButton());
-//     // otherwise, emit the signal to start the next round
-//     } else {
-//       rebloc.emit('nextround');
-//     }
-//   }
-// });
+_rebloc.nextIndex = function nextIndex() {
+  _rebloc.seqIdx++;
+  // If we we're still in range
+  if (_rebloc.seqIdx < _rebloc.sequence.length) {
+    return _rebloc.seqIdx;
+  }
 
-_rebloc.nextroundCallback = function() {
+  // Rollover to beginning
+  _rebloc.seqIdx = 0;
+
+  return false;
+};
+
+_rebloc.currentIndex = function currentIndex() {
+  return sequence[_rebloc.seqIdx];
+};
+
+_rebloc.onNextRound = function onNextRound() {
   // Creare a new sequence
-  _rebloc.newSequence();
-  // Begin Display
-  _rebloc.display(buttonIndex);
+  var i;
 
+  // Replace the indices in the sequence from last round
+  for (i = 0; i < _rebloc.rounds; i++) {
+    _rebloc.sequence[i] = _rebloc.randomIndex();
+  }
+
+  // Add one more index for this round
+  _rebloc.sequence.append(_rebloc.randomIndex());
+
+  _rebloc.rounds++;
+  _rebloc.delay--;
+
+  // Begin Display
+  _rebloc.display(_rebloc.currentIndex());
 };
 
-_rebloc.display = function(buttonIndex) {
-  trellis.led(buttonIndex.i, buttonIndex.j).output(1);
+_rebloc.display = function(idx) {
+  // Turn on the requested LED
+  _reblpc.trellis.led(idx.i, idx.j).output(1);
+
+  // Set a timeout for when to change the display
   setTimeout(function() {
+    // Turn off previous LED
+    _rebloc.trellis.led(idx.i, idx.j).output(0);
+
+    // Get the next element in the sequence
     if (_rebloc.nextIndex()) {
       _rebloc.display(_rebloc.currentIndex());
+    // If there are no more elements in the sequence, start acquiring
     } else {
-      _rebloc.resetSequencePtr();
       _rebloc.acquire(_rebloc.currentIndex());
     }
   }, _rebloc.delay);
@@ -73,12 +93,14 @@ _rebloc.pressCallback = function() {
 
 _rebloc.acquire = function(index) {
   // Setup an event listener for the specific button we need.
-  trellis.button(i, j).once('press', pressCallback);
+  _rebloc.trellis.button(i, j).once('press', pressCallback);
 
   _rebloc.lastTimeout = setTimeout(function() {
       // Remove the event listener
-      trellis.button(index.i, index.j).removeListener('press', _rebloc.pressCallback);
+      _rebloc.trellis.button(index.i, index.j).removeListener('press', _rebloc.pressCallback);
       // End the game
       rebloc.emit('gameover');
-  },_trellis.delay);
+  },_rebloc.delay);
 };
+
+module.exports = rebloc;
